156d6467aa8d476bdf4259d421d7969c7f59f9ed
Abhishek007Y <117890854+Abhishek007Y@users.noreply.github.com>
2026-03-16 12:38:38 +0530

    Delete badminton_league directory

--- badminton_league/app/models/membership.rb
+++
@@ -1,8 +0,0 @@
-class Membership < ApplicationRecord
-  belongs_to :user
-  belongs_to :league
-
-  enum :role, { member: 0, admin: 1 }
-
-  validates :user_id, uniqueness: { scope: :league_id, message: "is already in this league" }
-end
